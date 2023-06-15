2d115a4b3d7b721e410a656a93883955d9df9215
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-16 00:31:10 +0300

    Завершенно задание 2 и 6

---
+++ Task_2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+
+        public static List<string> rocerds = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("1 - додати елементи; 2 - Вивети данни на екран; 3 - зберегти елементи; 4 - Завантажити елементи;");
+                string menuNam = Console.ReadLine();
+
+                switch (menuNam)
+                {
+                    case "1":
+                        {
+                            for (int i = 1; i <= 10; i++)
+                            {
+                                rocerds.Add($"Hello Elemmen {i}");
+                            }
+
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.Clear();
+                            if (rocerds.Count == 0)
+                            {
+                                Console.WriteLine("Елементив немае тут пусто!");
+                            }
+
+                            foreach (var item in rocerds)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case "3":
+                        {
+                            Save();
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            Dowload();
+                            break;
+                        }
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+
+        }
+
+
+        public static void Save()
+        {
+
+            using (StreamWriter sw = new StreamWriter("seves.txt"))
+            {
+                foreach (var item in rocerds)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+
+
+        }
+
+        public static void Dowload()
+        {
+            using (StreamReader rd = new StreamReader("seves.txt"))
+            {
+                string line;
+                while ((line = rd.ReadLine()) != null)
+                {
+                    rocerds.Add(line);
+                }
+            }
+        }
+    }
+}
--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Task_6
 {
@@ -10,6 +12,35 @@ namespace Task_6
     {
         static void Main(string[] args)
         {
+            var directory = new DirectoryInfo("Test");
+
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                string name = $"Folder_{i}.txt";
+
+                FileStream newFile = File.Create(directory +"\\"+ name);
+
+                newFile.Close();
+            }
+
+            Console.WriteLine("Натисніть на кнопку щоб все відалити!");
+            Console.ReadKey();
+
+            FileInfo[] files = directory.GetFiles("*.txt");
+            foreach (var item in files)
+            {
+                item.Delete();
+            }
+            directory.Delete();
+
+            Console.ReadKey();
+
         }
     }
 }
