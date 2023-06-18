a52c46a72f43080d38a6de3e133ce8e258df46a3
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-18 15:06:23 +0300

    Завершени дз

---
+++ Task_3/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] disk = Directory.GetLogicalDrives();
+            foreach (var item in disk)
+            {
+                DriveBox.Items.Add(item);
+            }
+
+            DriveBox.SelectedIndex = 0;
+
+        }
+
+        private void DriveBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            SubDrirBox.Items.Clear();
+
+            string[] subdir = Directory.GetDirectories(DriveBox.SelectedItem as string);
+
+            SubDrirBox.Items.Add(DriveBox.SelectedItem as string);
+
+            foreach (var item in subdir)
+            {
+                SubDrirBox.Items.Add(item + @"\");
+            }
+
+            SubDrirBox.SelectedIndex = 0;
+
+
+        }
+
+        private void SkrinButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            FileTextBox.Text = null;
+
+
+            FileInfo file = new FileInfo($"{SubDrirBox.SelectedItem}" + $"{NameFileBox.Text}" + $".{ExtenshFileBox.Text}");
+
+
+
+            if (file.Exists)
+            {
+                FileStream openFile = File.Open(file.FullName, FileMode.OpenOrCreate, FileAccess.Read);
+                using (StreamReader read = new StreamReader(openFile))
+                {
+                    FileTextBox.Text += read.ReadToEnd();
+                }
+                openFile.Close();
+            }
+            else
+            {
+                FileTextBox.Text = "Тут нем такого файла " + $"{SubDrirBox.SelectedItem}" + $"{NameFileBox.Text}" + $".{ExtenshFileBox.Text}";
+            }
+
+        }
+
+
+        private void CompressedButton_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = $"{SubDrirBox.SelectedItem}" + $"{NameFileBox.Text}" + $".{ExtenshFileBox.Text}"; // Замените на путь к вашему файлу
+
+            // Проверка, существует ли файл
+            if (File.Exists(filePath))
+            {
+                CompressFile(filePath);
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден.");
+            }
+        }
+
+        void CompressFile(string filePath)
+        {
+            string compressedFilePath = filePath + ".gz"; // Путь к сжатому файлу
+
+            // Создание потока для сжатия данных
+            using (var inputFileStream = new FileStream(filePath, FileMode.Open))
+            using (var compressedFileStream = new FileStream(compressedFilePath, FileMode.Create))
+            {
+                // Использование GZipStream для сжатия данных
+                using (var gzipStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                {
+                    // Копирование данных из исходного файла в сжатый файл
+                    inputFileStream.CopyTo(gzipStream);
+                }
+            }
+
+            MessageBox.Show("Файл успешно сжат. Путь к сжатому файлу: " + compressedFilePath);
+        }
+    }
+}
---
+++ Task_4/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        IsolatedStorageFile userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+        IsolatedStorageFileStream userStream;
+        private void CrieteFileButtin_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream($"{NameFile.Text}.{ExtensionFile.Text}", FileMode.Create, userStorage);
+
+            using (StreamWriter userWriter = new StreamWriter(userStream))
+            {
+                userWriter.WriteLine(ContentFile.Text);
+            }
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string[] files = userStorage.GetFileNames($"{NameFile.Text}.{ExtensionFile.Text}");
+
+            if (files.Length == 0)
+            {
+                ContentFile.Text = "No data saved for this user";
+            }
+            else
+            {
+                // Прочитати дані з потоку.
+                userStream = new IsolatedStorageFileStream($"{NameFile.Text}.{ExtensionFile.Text}", FileMode.Open, userStorage);
+
+                using (StreamReader userReader = new StreamReader(userStream))
+                {
+                    ContentFile.Text = userReader.ReadToEnd();
+                }
+
+            }
+
+        }
+
+    }
+}
